7e217ca3df14b5cc68fce87f2ae84727d25c9a3e
YassineBDH <boudouh.yassine@gmail.com>
2024-04-24 13:19:47 +0200

    initDB + creation des tables user role et la relation user_role

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const app = require('./src/app');
+const { initDB } = require('./src/db/initDB');
 
 const PORT = 3001
 
 app.listen(PORT, () => {
 	console.log('Server is listening', { PORT });
+	initDB();
 });
---
+++ src/db/initDB.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const {KNEX} = require('./connexion');
+
+async function initDB() {
+    const connexion = await KNEX;
+    if (!await connexion.schema.hasTable('user')){
+        console.log('creation de la table user');
+        await connexion.schema.createTable('user',table => {
+            table.increments('id');
+            table.string('nom');
+            table.string('prenom');
+            table.string('email');
+            table.string('password');
+            table.unique('id');
+            table.unique('email');
+        });
+
+    }
+    if (!await connexion.schema.hasTable('role')){
+        console.log('creation de la table role');
+        await connexion.schema.createTable('role',table => {
+            table.increments('id');
+            table.string('nom');
+            table.unique(['id', 'nom']);
+            table.unique('id');
+            table.unique('nom');
+        });
+    }
+    if (!await connexion.schema.hasTable('user_role')){
+        console.log('creation de la table user_role');
+        await connexion.schema.createTable('user_role',table => {
+            table.string('user_id');
+            table.string('role_id');
+            table
+            .foreign("user_id")
+            .references("user.id")
+            .onDelete("CASCADE");
+            table
+            .foreign("role_id")
+            .references("role.id")
+            .onDelete("CASCADE");
+        });
+    }
+    
+}
+module.exports= {initDB};
